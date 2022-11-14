34685de42154ff0645bdc69fb2d641db597ece4d
Jeff Scott <jpscott@ranken.edu>
2022-11-14 13:52:16 -0600

    Start of router-v6 project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+		<Routes>
+			<Route path="/apps" element={<Navigate replace to="/courses" />}/>
+			<Route path="/courses" element={<Courses />}/>
+		</Routes>
+	  </Router>
     </div>
   );
 }
