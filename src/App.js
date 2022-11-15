fba3f3d9bc11990f6fed70c82ebb650c0bc3f731
Jeff Scott <jpscott@ranken.edu>
2022-11-15 11:01:15 -0600

    First 5+ pages of react-router-v6 article

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './components/Home'
+import Courses from './components/Courses'
 
 function App() {
   return (
     <div className="App">
       <Router>
 		<Routes>
+			<Route path="/" element={<Home />}/>
 			<Route path="/apps" element={<Navigate replace to="/courses" />}/>
 			<Route path="/courses" element={<Courses />}/>
 		</Routes>
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -5,10 +5,10 @@ const Courses = () => {
 		<div className="container">
 			<h1>My Courses</h1>
 				<ul className="list-group">
-					<li className="list-group-item">AWD1000 - Web Development Technologies"></li>
-					<li className="list-group-item">AWD1100 - Programming Fundamentals with C# "></li>
-					<li className="list-group-item">AWD1111 - Database Driven Web Development I"></li>
-					<li className="list-group-item">AWD1115 - Database Driven Web Development II"></li>
+					<li className="list-group-item">AWD1000 - Web Development Technologies</li>
+					<li className="list-group-item">AWD1100 - Programming Fundamentals with C#</li>
+					<li className="list-group-item">AWD1111 - Database Driven Web Development I</li>
+					<li className="list-group-item">AWD1115 - Database Driven Web Development II</li>
 				</ul>
 		</div>
 		
